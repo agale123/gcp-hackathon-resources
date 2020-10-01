89b61209353d1cda0217bf48b82af4a3c3d65803
Alison <agale753@gmail.com>
2020-10-01 11:53:53 -0700

    Update http request example to use the new fetch method

--- simple-app-engine-with-cloud-function/www/index.js
+++ simple-app-engine-with-cloud-function/www/index.js
@@ -1,15 +1,11 @@
 // Take the input text and translate to French.
 function translate() {
     let text = encodeURI(document.getElementById('input').value);
-    const Http = new XMLHttpRequest();
-    const url=`https://us-central1-simple-232901.cloudfunctions.net/translate?message=${text}`;
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange=(e)=>{
-        document.getElementById('output').innerText = Http.responseText;
-    }
-    
-
+    fetch(`https://us-central1-simple-232901.cloudfunctions.net/translate?message=${text}`)
+        .then(response => response.text())
+        .then(response => {
+            document.getElementById('output').innerText = response;
+        });
 }
 
 document.getElementById('button').addEventListener('click', translate);
